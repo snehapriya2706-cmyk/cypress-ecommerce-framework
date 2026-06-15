80eb76209f949a1323fdd38225a2b40430a7b2f0
snehamurali1611-max <snehamurali1611@gmail.com>
2026-06-15 18:01:13 +0530

    Update Cypress configuration and ignore generated files

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require("cypress");
 
+module.exports = defineConfig({
+  video: true,
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+/*const { defineConfig } = require("cypress");
+
 
 module.exports = defineConfig({
   projectId: '5pmreo',
